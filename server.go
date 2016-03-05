36139f8e94d46c460678d01e2d7f6372df2df1ec
John J <johnrjj@gmail.com>
2016-03-05 17:49:59 -0500

    Fixed so each goroutine does not make an instance of the redis client, instead we can pool it across the goroutines

--- server.go
+++ server.go
@@ -3,23 +3,29 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"runtime"
+
 	"gopkg.in/redis.v3"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	//fmt.Fprint(w, "Hello World!");
-	client := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-		Password: "",
-		DB: 0,
-	})
+type Server struct {
+	db *redis.Client
+}
 
-	results, _ := client.HGetAll("keystone:dynatron/metric::ebis-wlri-analysis:info").Result()
+func (s *Server) handler(w http.ResponseWriter, r *http.Request) {
+	results, _ := s.db.HGetAll("keystone:dynatron/metric::ebis-wlri-analysis:info").Result()
 	fmt.Fprint(w, results)
-	//fmt.Printf("The type is: %s \n", results)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":3000", nil);
+	runtime.GOMAXPROCS(1)
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	server := &Server{db: client}
+
+	http.HandleFunc("/", server.handler)
+	http.ListenAndServe(":3000", nil)
 }
